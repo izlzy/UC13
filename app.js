a307f1bdde457586c6201533db0368887fe2ccd5
Joao <joao.lajunior@senacsp.edu.br>
2025-10-01 21:46:36 -0300

    Extensao MySql2 e conexao com o banco

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express ();
+const mysql = require('mysql2');
 
-app.get ("/" , function (req, res){
-    res.write ("Hello World");
-    res.end();
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'senac',
+    port: 3306,
+    database: 'ecommerce_pc'
+});
+
+
+conexao.connect((erro) => {
+    if (erro) {
+        console.error('Erro ao Conectar ao Banco de dados', erro);
+        return;
+    }
+    console.log('Conexao com o bando de dados estabelecida com sucesso!');
 });
 
 app.listen(8080);
+
---
+++ script.sql
@@ -0,0 +1,73 @@
+-- Criação do banco de dados
+CREATE DATABASE IF NOT EXISTS ecommerce_pc
+    DEFAULT CHARACTER SET = utf8mb4;
+
+USE ecommerce_pc;
+
+-- Tabela de clientes
+CREATE TABLE clientes (
+    cod_id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(100) NOT NULL,
+    endereco TEXT,
+    criado_em DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Tabela de categorias
+CREATE TABLE categorias (
+    cod_id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL UNIQUE,
+    descricao TEXT
+);
+
+-- Tabela de produtos
+CREATE TABLE produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT,
+    preco DECIMAL(10,2) NOT NULL,
+    estoque INT DEFAULT 0,
+    categoria_id INT,
+    criado_em DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (categoria_id) REFERENCES categorias(cod_id)
+);
+
+-- Tabela de pedidos
+CREATE TABLE pedidos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT,
+    data DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50) DEFAULT 'Em Andamento',
+    FOREIGN KEY (cliente_id) REFERENCES clientes(cod_id)
+);
+
+-- Tabela de itens do pedido
+CREATE TABLE item_pedido (
+    cod_id INT AUTO_INCREMENT PRIMARY KEY,
+    pedido_id INT,
+    produto_id INT,
+    quantidade INT DEFAULT 1,
+    preco_unitario DECIMAL(10,2),
+    FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
+    FOREIGN KEY (produto_id) REFERENCES produtos(id)
+);
+
+INSERT INTO categoria (nome, descricao)
+VALUES 
+  ('Processadores', 'CPUs para desktop'),
+  ('Memoria RAM', 'Módulos de memória para PCs'),
+  ('Placas de Vídeo', 'GPUs para jogos e renderização'),
+  ('Armazenamento', 'Dispositivos como SSDs e HDs'),
+  ('Placas-Mãe', 'Motherboards compatíveis com CPUs e periféricos');
+
+
+
+INSERT INTO produtos (nome,descricao,preco,estoque,categoria_id)
+VALUES
+    ('Intel Core i5-10400 - 6 nucleos - 12 threads - 2.9Ghz - R$899,90-estoque:10-categoria:1(Processadores)'),
+    ('AMD Ryzen 5 5600x - 6 nucleos - 12 threads - 4.6GHz - R$1099,99-estoque:20-categoria:2(Processadores)'),
+    ('Memoria DDR4 8GB 2666MHz - Kingston Fury Beast- R$189,90-estoque:20-categoria:2(Memoria Ram)'),
+    ('Memoria DDR4 16GB 3200MHz - Corsair Vengeance LPX- R$349,50 - estoque:15-categoria:2(Memoria Ram)'),
+    ('NVIDIA GeForce GTX 1660 6GB GDDR5 - R$1450,00-estoque:5-categoria:3(Placas de Video)');
+  SELECT * FROM categoria
